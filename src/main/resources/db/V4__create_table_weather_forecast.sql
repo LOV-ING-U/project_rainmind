58e4e042cbf7035ee883ce61eefd74124b5028cb
SugAr <kkwandy@naver.com>
2025-12-22 23:35:44 +0900

    fix schema

--- src/main/resources/db/V4__create_table_weather_forecast.sql
+++ src/main/resources/db/V4__create_table_weather_forecast.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS weather_forecast
 (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    region_name VARCHAR(128) NOT NULL,
-    POP BIGINT NOT NULL,
-    PTY BIGINT NOT NULL,
-    PCP BIGINT NOT NULL,
-    SKY BIGINT NOT NULL,
-    WSD DECIMAL(3, 1) NOT NULL,
+    region_code BIGINT NOT NULL,
+    pop TINYINT NOT NULL,
+    pty TINYINT NOT NULL,
+    pcp TEXT NOT NULL,
+    sky TINYINT NOT NULL,
+    wsd DECIMAL(4, 1) NOT NULL,
     base_date_and_time TIMESTAMP(6) NOT NULL,
     fcst_date_and_time TIMESTAMP(6) NOT NULL,
     fetched_at TIMESTAMP(6) NOT NULL,
 
-    UNIQUE KEY weather_forecast__uk_ (region_name, fcst_date_and_time),
-    CONSTRAINT weather_forecast__fk_region_name FOREIGN KEY (region_name) REFERENCES location (region_name) ON DELETE CASCADE
+    UNIQUE KEY weather_forecast__uk_ (region_code, fcst_date_and_time),
+    CONSTRAINT weather_forecast__fk_region_code FOREIGN KEY (region_code) REFERENCES location (id) ON DELETE CASCADE
 );
