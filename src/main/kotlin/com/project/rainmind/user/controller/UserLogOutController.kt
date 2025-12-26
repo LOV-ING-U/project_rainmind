8b3b3bc7b7b1f1d7ddc9ca9435833b7682c91401
SugAr <kkwandy@naver.com>
2025-12-26 16:01:46 +0900

    finish logout + redis TTL logic

--- src/main/kotlin/com/project/rainmind/user/controller/UserLogOutController.kt
+++ src/main/kotlin/com/project/rainmind/user/controller/UserLogOutController.kt
@@ -1,23 +1,35 @@
 package com.project.rainmind.user.controller
 
+import com.project.rainmind.user.jwt.JwtTokenProvider
 import com.project.rainmind.user.service.UserLogOutService
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class UserLogOutController (
     @Autowired
-    private val userLogOutService: UserLogOutService
+    private val userLogOutService: UserLogOutService,
+    private val jwtTokenProvider: JwtTokenProvider
 ) {
     @PostMapping("/v1/user/logout")
     fun logout(
-        request: HttpServletRequest
+        @RequestHeader(name = "Authorization") authHeader: String
     ): ResponseEntity<Void> {
-        val tokenFromRequest = request.getAttribute("token") as String
-        userLogOutService.logout(tokenFromRequest)
+        // http servlet request 헤더만 받는다.
+        // 토큰 추출
+        val token = authHeader
+            .takeIf { it.startsWith("Bearer ") }
+            ?.removePrefix("Bearer ")
+            ?: return ResponseEntity.noContent().build()
+
+        // 이미 만료된 토큰이면, 에러 처리 안하고 그냥 pass
+        if(!jwtTokenProvider.tokenValidateCheck(token)) return ResponseEntity.noContent().build()
+
+        userLogOutService.logout(token)
         return ResponseEntity.ok().build()
     }
 }
--- src/main/kotlin/com/project/rainmind/user/service/UserLogOutService.kt
+++ src/main/kotlin/com/project/rainmind/user/service/UserLogOutService.kt
@@ -1,16 +1,25 @@
 package com.project.rainmind.user.service
 
+import com.project.rainmind.user.jwt.JwtTokenProvider
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import java.util.concurrent.TimeUnit
 
 @Service
 class UserLogOutService (
-    private val stringRedisTemplate: StringRedisTemplate
+    @Autowired
+    private val stringRedisTemplate: StringRedisTemplate,
+    private val jwtTokenProvider: JwtTokenProvider
 ) {
     fun logout(
         token: String
     ) {
+        // controller 에서 추출한, 인증된 토큰을 건네받아 인증 & redis 에 등록(TTL)
         // opsForValue => string 연산(redis 에서) 처리하는 interface 반환받음
-        stringRedisTemplate.opsForValue().set("blacklist:$token", "expired")
+        // 토큰 자체를 key 로 만들면: 보통 GET key로 조회하므로 바로 조회가능(덮어씜 방지)
+        val ttl_ms = jwtTokenProvider.getRemainTimeOfTokenMS(token)
+        stringRedisTemplate.opsForValue().set("jwt:blacklist:$token", "notUsed", ttl_ms, TimeUnit.MILLISECONDS)
     }
 }
