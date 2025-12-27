6a4d333816bbe65e61ea158b309b4d9b9b050f28
SugAr <kkwandy@naver.com>
2025-12-28 00:11:02 +0900

    finish external fetch logic

--- src/main/kotlin/com/project/rainmind/weather/weatherfetch/KmaExternalFetchClient.kt
+++ src/main/kotlin/com/project/rainmind/weather/weatherfetch/KmaExternalFetchClient.kt
@@ -1,5 +1,6 @@
 package com.project.rainmind.weather.weatherfetch
 
+import com.project.rainmind.weather.weatherfetch.dto.KmaWeatherFutureFetchResponse
 import com.project.rainmind.weather.weatherfetch.dto.KmaWeatherNowFetchResponse
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
@@ -10,6 +11,7 @@ class KmaExternalFetchClient (
     private val weatherNowFetchClient: RestClient,
     @Value("\${kma.auth-key}") private val serviceKey: String
 ) {
+    // 초단기실황조회
     fun fetchNowWeather(
         baseDate: String,
         baseTime: String,
@@ -31,6 +33,30 @@ class KmaExternalFetchClient (
                     .queryParam("ny", ny).build()
             }.retrieve() // retrieve : 실제 요청 보내고, 아래에서 JSON 응답 역직렬화해서 DTO로 받는다.
             .body(KmaWeatherNowFetchResponse::class.java)
+            ?: throw ExternalWeatherFetchErrorException()
+    }
+
+    // 단기예보조회
+    fun fetchFutureWeather(
+        baseDate: String,
+        baseTime: String,
+        nx: Int,
+        ny: Int
+    ): KmaWeatherFutureFetchResponse {
+        return weatherNowFetchClient.get()
+            .uri { builder ->
+                builder
+                    .path("/1360000/VilageFcstInfoService_2.0/getVilageFcst")
+                    .queryParam("serviceKey", serviceKey)
+                    .queryParam("pageNo", 1)
+                    .queryParam("numOfRows", 1000)
+                    .queryParam("dataType", "JSON")
+                    .queryParam("base_date", baseDate)
+                    .queryParam("base_time", baseTime)
+                    .queryParam("nx", nx)
+                    .queryParam("ny", ny).build()
+            }.retrieve()
+            .body(KmaWeatherFutureFetchResponse::class.java)
             ?: throw ExternalWeatherFetchErrorException()
     }
 }
