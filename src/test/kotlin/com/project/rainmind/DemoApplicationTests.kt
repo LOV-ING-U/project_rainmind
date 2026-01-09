1b58dbcd5fefd5da58e2c747dd036751bc5ad029
SugAr <kkwandy@naver.com>
2026-01-10 00:44:10 +0900

    fix test logic

--- src/test/kotlin/com/project/rainmind/DemoApplicationTests.kt
+++ src/test/kotlin/com/project/rainmind/DemoApplicationTests.kt
@@ -1,10 +1,12 @@
 package com.project.rainmind
 
+import com.project.rainmind.helper.BaseIntegrationTestContainers
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DemoApplicationTests {
+class DemoApplicationTests: BaseIntegrationTestContainers() {
 	@Test
 	fun contextLoads() {
 	}
